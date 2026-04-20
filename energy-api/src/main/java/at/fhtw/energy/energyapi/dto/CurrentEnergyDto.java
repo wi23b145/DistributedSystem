85fe7e563adabc3dfbce625059c8718ff124b834
Kiyavash <wi23b159@technikum-wien.at>
2026-04-20 21:27:05 +0200

    Added HistoricalEnergyDto and CurrentEnergyDto

--- energy-api/src/main/java/at/fhtw/energy/energyapi/dto/CurrentEnergyDto.java
+++ energy-api/src/main/java/at/fhtw/energy/energyapi/dto/CurrentEnergyDto.java
@@ -1,5 +1,17 @@
 package at.fhtw.energy.energyapi.dto;
 
 public class CurrentEnergyDto {
+    private String hour;
+    private double communityDepleted;
+    private double gridPortion;
 
+    public CurrentEnergyDto(String hour, double communityDepleted, double gridPortion) {
+        this.hour = hour;
+        this.communityDepleted = communityDepleted;
+        this.gridPortion = gridPortion;
+    }
+
+    public String getHour() { return hour; }
+    public double getCommunityDepleted() { return communityDepleted; }
+    public double getGridPortion() { return gridPortion; }
 }
--- energy-api/src/main/java/at/fhtw/energy/energyapi/dto/HistoricalEnergyDto.java
+++ energy-api/src/main/java/at/fhtw/energy/energyapi/dto/HistoricalEnergyDto.java
@@ -1,5 +1,22 @@
+
 package at.fhtw.energy.energyapi.dto;
 
 public class HistoricalEnergyDto {
+    private String hour;
+    private double communityProduced;
+    private double communityUsed;
+    private double gridUsed;
 
+    public HistoricalEnergyDto(String hour, double communityProduced,
+                               double communityUsed, double gridUsed) {
+        this.hour = hour;
+        this.communityProduced = communityProduced;
+        this.communityUsed = communityUsed;
+        this.gridUsed = gridUsed;
+    }
+
+    public String getHour() { return hour; }
+    public double getCommunityProduced() { return communityProduced; }
+    public double getCommunityUsed() { return communityUsed; }
+    public double getGridUsed() { return gridUsed; }
 }
