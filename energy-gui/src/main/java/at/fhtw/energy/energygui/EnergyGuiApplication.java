7f58e53ac8c519efc4e17e8bb4eb063373522762
wi23b006 <wi23b006@technikum-wien.at>
2026-04-20 20:47:02 +0200

    Added EnergyGuiApplication and energy-view.fxml

--- energy-gui/src/main/java/at/fhtw/energy/energygui/EnergyGuiApplication.java
+++ energy-gui/src/main/java/at/fhtw/energy/energygui/EnergyGuiApplication.java
@@ -1,6 +1,19 @@
 package at.fhtw.energy.energygui;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class EnergyGuiApplication {
+import java.io.IOException;
 
+public class EnergyGuiApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(EnergyGuiApplication.class.getResource("energy-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Energy Community Monitor");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
