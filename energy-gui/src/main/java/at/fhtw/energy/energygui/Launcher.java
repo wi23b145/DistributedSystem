247fa7af55e427ecc5d2ab4f30c045719a9fc730
wi23b145 <wi23b145@technikum-wien.at>
2026-06-16 17:18:32 +0200

    Finalized Project

--- energy-gui/src/main/java/at/fhtw/energy/energygui/Launcher.java
+++
@@ -1,9 +0,0 @@
-package at.fhtw.energy.energygui;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(EnergyGuiApplication.class, args);
-    }
-}
